992144033f9d11acc986bccf5e717e80b457f0b0
marisofip <marisofipalencia@gmail.com>
2022-12-26 15:53:13 +0000

    creando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState("")
+    const [listaTareas, setListaTareas] = useState([])
+    const [listaVacia, setListaVacia] = useState ("No hay tareas")
+
+	function cargarTarea(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            setListaTareas([
+                ...listaTareas,
+                tarea
+            ]); // suma la nueva sin borrar lo anterior
+            console.log(listaTareas);
+            setTarea("");
+            setListaVacia("")
+        } 
+
+    }
+	function eliminarTarea(id) {
+        let sinEliminado = [];
+        sinEliminado = listaTareas.filter((item,index) => {
+            if (index !== id)  {
+                return item
+            } 
+            if (listaVacia == "" && listaTareas.length == 1 ) {
+                setListaVacia("No hay tareas")
+            } 
+        } ) 
+        setListaTareas(sinEliminado);
+    }
+      
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+	
+
+			<h1> TO-DO LIST </h1>
+			<div className="container-fluid">
+	    <input type="text" className="form-control" placeholder="Escribir Tarea" aria-label="Username" value={tarea}
+                    onChange={(e) => setTarea(e.target.value)}
+                    onKeyDown={cargarTarea}/>
+     	<ul class="list-group list-group-flush">
+		 {listaTareas.map((item, id) => 
+                    <li className = "list-group-item hidden" key={id}>{item} 
+                    <button type="button" className="btn btn-outline-primary float-end" 
+                    onClick={()=> eliminarTarea(id)}>x
+                    </button>
+					</li> ) }
+			  </ul>
+			{listaVacia}
+			</div>
+			</div>
+	
+
+
+
 	);
 };
 
