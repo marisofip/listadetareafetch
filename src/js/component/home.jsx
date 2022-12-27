c964218e6748cf7fc0ed1a4a7fdb63cfe3600302
marisofip <marisofipalencia@gmail.com>
2022-12-27 20:55:06 +0000

    despues de sufrir lo consegui

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 
 //include images into your bundle
@@ -6,59 +6,154 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+   
+	const [tarea, setTarea] = useState([])
+	console.log("newtarea:", tarea)
+	const [tareaInput, setTareaInput] = useState({label:"",done:false});
+	const [userActive, setUserActive] = useState(false);
+	
 
-	const [tarea, setTarea] = useState("")
-    const [listaTareas, setListaTareas] = useState([])
-    const [listaVacia, setListaVacia] = useState ("No hay tareas")
+   	const addTask = (e) => {
+		e.preventDefault();
+		if (tareaInput.label !== ""){
+      newTodo();      
+			setTarea([...tarea, tareaInput]);
+			setTareaInput({label:"",done:false});
+		}
+	} 
+	const deleteTask = (index) => {
+		const newTask = [...tarea];
+		newTask.splice(index, 1);
+		setTarea(newTask);		
+    console.log("delete:", tarea)
+	}
 
-	function cargarTarea(e) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
 
-            setListaTareas([
-                ...listaTareas,
-                tarea
-            ]); // suma la nueva sin borrar lo anterior
-            console.log(listaTareas);
-            setTarea("");
-            setListaVacia("")
-        } 
+	useEffect(() => {
+		user();
+		if(!userActive) {
+		  newUser();           
+		}
+		else{
+		  getTodo();
+		}
+		}, [!userActive]);
+	
+		const newUser = () => {
+			fetch( "https://assets.breatheco.de/apis/fake/todos/user/marisofip", {
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+				  "Content-Type": "application/json"
+				}
+			  })
+			  .then(resp => {
+				console.log("Response newUser ok", resp.ok);
+				console.log("Response newUser status", resp.status); 
+			})
+			.then(data => {
+				console.log("newUser data", data); })}
 
-    }
-	function eliminarTarea(id) {
-        let sinEliminado = [];
-        sinEliminado = listaTareas.filter((item,index) => {
-            if (index !== id)  {
-                return item
-            } 
-            if (listaVacia == "" && listaTareas.length == 1 ) {
-                setListaVacia("No hay tareas")
-            } 
-        } ) 
-        setListaTareas(sinEliminado);
-    }
-      
+
+				const user = () => {
+					fetch('https://assets.breatheco.de/apis/fake/todos/user', {
+					method: "GET",    
+					})
+					.then(resp => {
+						console.log("Response User ok", resp.ok);
+						console.log("Response User status", resp.status); // el código de estado = 200 o código = 400 etc.		
+							return resp.json() })	
+					.then(data => {		      
+						console.log("user data", data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+						setUserActive(data.includes("marisofip"));
+						console.log("setUserActive status", data.includes("marisofip") );               		
+					})
+				 }
+
+
+				 const getTodo = () => {
+					fetch('https://assets.breatheco.de/apis/fake/todos/user/marisofip', {
+					method: "GET",      
+				  })
+				  .then(resp => {
+					  console.log("Response getTodo ok", resp.ok); // Será true (verdad) si la respuesta es exitosa.
+					  console.log("Response getTodo status", resp.status); 
+					  return resp.json(); 
+				  })
+				  .then(data => {
+					   console.log("getTodo data", data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+					 
+					  setTarea(data);        
+				  }, [])
+				 
+				}  
+				
+					const newTodo = () => {      
+					fetch('https://assets.breatheco.de/apis/fake/todos/user/marisofip', {
+					method: "PUT",
+					body: JSON.stringify(tarea),
+					headers: {
+					  "Content-Type": "application/json"
+					}
+				  })
+				  .then(resp => {
+					  console.log("Response newTodo ok", resp.ok); // Será true (verdad) si la respuesta es exitosa.
+					  console.log("Response newTodo status", resp.status); // el código de estado = 200 o código = 400 etc.
+					  return resp.json(); 
+				  })
+				  .then(data => {
+					  console.log("newTodo data", data); //esto imprimirá en la consola el objeto exacto recibido del servidor        
+					  console.log("Tareas:", tarea);
+					             
+				  })}				
+				
+			  
+				  const deleteAll = () => {fetch('https://assets.breatheco.de/apis/fake/todos/user/marisofip', {
+					method: "DELETE",
+					body: JSON.stringify([tarea]),
+					headers: {
+					  "Content-Type": "application/json"
+					}
+				  })
+				  .then(resp => {
+					  console.log("Response deleteAll ok", resp.ok); // Será true (verdad) si la respuesta es exitosa.
+					  console.log("Response deleteAll status", resp.status); 
+					  return resp.json(); 
+				  })
+				  .then(data => {
+					  //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+					  console.log("deleteAll data", data); 
+					  if (data.result == "ok"){
+						setTarea([]);
+						setUserActive(false);
+					  }      
+				  })}
 
 	return (
 		<div className="container">
-	
-
-			<h1> TO-DO LIST </h1>
+				<h1> TO-DO LIST </h1>
 			<div className="container-fluid">
-	    <input type="text" className="form-control" placeholder="Escribir Tarea" aria-label="Username" value={tarea}
-                    onChange={(e) => setTarea(e.target.value)}
-                    onKeyDown={cargarTarea}/>
-     	<ul class="list-group list-group-flush">
-		 {listaTareas.map((item, id) => 
-                    <li className = "list-group-item hidden" key={id}>{item} 
-                    <button type="button" className="btn btn-outline-primary float-end" 
-                    onClick={()=> eliminarTarea(id)}>x
-                    </button>
-					</li> ) }
-			  </ul>
-			{listaVacia}
-			</div>
-			</div>
+				<form  className = "tareas" onClick={addTask}>
+				<ul className="list-group list-group-flush">	
+							
+	    <input type="text" className="form-control"  value={tareaInput.label} placeholder="Escribir Tarea" aria-label="Username" 
+                    onChange={e => setTareaInput({label: e.target.value, done: false})}/>
+     	
+		{tarea.map((tarea,index) => (
+      <div className="task" key={index}> 
+        <div className="newItem" key={index}> <li  className="list-group-item" >{tarea.label} 
+        <button id="boton" className=" delete btn btn-outline-primary float-end"  onClick={() => deleteTask(index)}>x</button> 
+		</li>
+		</div>
+		</div>
+		))}
+			<p>Pendientes: {tarea.length}</p>
+			<button className="deleteall" onClick={deleteAll}>Borrar Todo</button>
+			
+			</ul>
+		</form>
+		</div>
+		</div>
 	
 
 
